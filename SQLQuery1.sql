c78f0d6858fb073dd62e643904b5d922d5eac4c7
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-04-12 21:16:44 +0700

    Create SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+/*Create Database Quan_ly_SV;*/
+	
+/*Create Database MyDB
+On(
+Name = MyDB_Data,
+FileName = 'D:\Database\MyDB_Data.mdf',
+Size = 1MB,
+Maxsize = 10MB,
+FileGrowth = 10%
+)*//*DROP Database Quan_ly_SV;*//*USE Quan_ly_SV;*/
